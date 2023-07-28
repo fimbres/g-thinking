72641ee17ed8b2511c6cf30ec50c813f02de256a
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-07-27 23:13:02 -0700

    new Heading component

---
+++ components/Heading.tsx
@@ -0,0 +1,35 @@
+import { cn } from '@/lib/utils';
+import { IconProps } from '@radix-ui/react-icons/dist/types';
+import React from 'react'
+
+interface HeadingProps {
+    title: string;
+    description: string;
+    icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<SVGSVGElement>>;
+    iconColor?: string;
+    bgColor?: string;
+}
+
+const Heading: React.FC<HeadingProps> = ({
+    title,
+    description,
+    icon: Icon,
+    iconColor,
+    bgColor,
+}) => {
+    return (
+        <>
+            <div className='px-4 lg:px-8 flex items-center gap-x-3 mb-8'>
+                <div className={cn('p-3 w-fit rounded-md', bgColor)}>
+                    <Icon className={cn('w-10 h-10', iconColor)}/>
+                </div>
+                <div>
+                    <h2 className='text-3xl font-bold'>{title}</h2>
+                    <p className='text-md text-muted-foreground'>{description}</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Heading
